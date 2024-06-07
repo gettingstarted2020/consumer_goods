272a55f09d645c1cbae4442436eba131a5c6ceca
Jaswanthi Mamidisetty <70503328+gettingstarted2020@users.noreply.github.com>
2024-06-07 18:39:25 +0530

    Update request 1.sql

--- request 1.sql
+++ request 1.sql
@@ -1,3 +1,6 @@
+/* task-1 Provide the list of markets in which customer "Atliq Exclusive" operates its
+business in the APAC region.*/
+  
 Select distinct market 
 from dim_customer
 where region ="APAC" and customer ="Atliq exclusive "
